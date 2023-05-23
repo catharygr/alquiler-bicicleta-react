16728c51ee42129c28ca7921417afb7ecd289178
catharygr <103951447+catharygr@users.noreply.github.com>
2023-05-23 18:55:53 +0100

    Trabajando con el componente App y css

---
+++ empresaAlquiler.js
@@ -0,0 +1,44 @@
+const empresasAlquiler = [
+  {
+    nombre: "Lokoloka",
+    contacto: "lokoloka.sl@gmail.com",
+    accesorios: {
+      adultosSolos: "Bicicleta eléctrica",
+      adultosConNiños: "Bicicleta con sillas",
+      adultosConAnimales: "Bicicleta con carro",
+    },
+    servicios: "De lunes a domingos",
+  },
+  {
+    nombre: "Capital Bikes",
+    contacto: "capitalbikes@gmail.com",
+    accesorios: {
+      adultosSolos: "Bicicleta eléctrica",
+      adultosConNiños: "Bicicleta con sillas",
+      adultosConAnimales: "Bicicleta con carro",
+    },
+    servicios: "De lunes a domingos",
+  },
+  {
+    nombre: "EbikeVialki",
+    contacto: "info@ebikevialki.net",
+    accesorios: {
+      adultosSolos: "Bicicleta eléctrica",
+      adultosConNiños: "Bicicleta con sillas",
+      adultosConAnimales: "Bicicleta con carro",
+    },
+    servicios: "De lunes a domingos",
+  },
+  {
+    nombre: "Guiartu",
+    contacto: "guiartuvitoria@gmail.com",
+    accesorios: {
+      adultosSolos: "Bicicleta eléctrica",
+      adultosConNiños: "Bicicleta con sillas",
+      adultosConAnimales: "Bicicleta con carro",
+    },
+    servicios: "De lunes a domingos",
+  },
+];
+
+export default empresasAlquiler;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+// import empresasAlquiler from "../empresaAlquiler";
 import "../style.css";
 
-function App() {}
+function App() {
+  return (
+    <div className="container">
+      <img className="logo" src="./image/logo.png" alt="" />
+      <h2 className="tituloq">Empresas de Alquiler de bicicleta</h2>
+    </div>
+  );
+}
 
 export default App;
