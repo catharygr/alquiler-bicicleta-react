9ddad8a57afcda23e601f18bafbe8baef497f6e3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-05-24 18:57:21 +0100

    Trabajando en el css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ function App() {
     <div className="container">
       <img className="logo" src="./image/cathy.png" alt="" />
       <h2 className="titulo">Somo los campeones</h2>
+      <div className="text-button">
+        <div className="textarea">
+          <textarea
+            placeholder="Escribe su coemntario aquí"
+            rows={3}
+            cols={20}
+          />
+        </div>
+        <button>Pública</button>
+      </div>
+      <h2 className="endosos">-Endosos-</h2>
     </div>
   );
 }
